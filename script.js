3d913a4e4cfe0c9674fc7682f328ffea7c5b683e
Абдурахманов Зафаржон <109537671+abdurakhmanov05@users.noreply.github.com>
2022-09-24 12:29:34 +0500

    seasons
    seasons

---
+++ script.js
@@ -0,0 +1,42 @@
+let month = +prompt('Месяц')
+
+let January = 1
+let February = 2 
+let March = 3
+let April = 4 
+let May = 5 
+let June = 6 
+let July = 7 
+let August = 8 
+let September = 9 
+let October = 10 
+let November = 11
+let December = 12 
+
+if(month === 1){
+    alert('Winter')
+}else if(month === 2){
+    alert('Winter')
+}else if(month === 3){
+    alert('Spring')
+}else if(month === 4){
+    alert('Spring')
+}else if(month === 5){
+    alert('Spring')
+}else if(month === 6){
+    alert('Summer')
+}else if(month === 7){
+    alert('Summer')
+}else if(month === 8){
+    alert('Summer')
+}else if(month === 9){
+    alert('Autumn')
+}else if(month === 10){
+    alert('Autumn')
+}else if(month === 11){
+    alert('Autumn')
+}else if(month === 12){
+    alert('Winter')
+}else(
+    alert('I don`t know')
+)
